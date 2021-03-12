012e5206f45a2780703c87bc44721a562591f794
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2021-03-12 18:30:47 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/thiagoribeiro/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.thiagoribeiro.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.thiagoribeiro.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+	}
+	
+	public UserDTO(User obj) {
+		this.id = obj.getId();
+		this.email = obj.getEmail();
+		this.name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}

ff65ed251f74157490f677c1b32fc0872b2a651a
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2021-03-29 18:31:56 -0300

    Consulta simples com query methods

--- src/main/java/com/thiagoribeiro/workshopmongo/resources/PostResource.java
+++ src/main/java/com/thiagoribeiro/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.thiagoribeiro.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagoribeiro.workshopmongo.domain.Post;
+import com.thiagoribeiro.workshopmongo.resources.resources.util.URL;
 import com.thiagoribeiro.workshopmongo.services.PostService;
 
 @RestController
@@ -23,4 +27,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findBytitle(@RequestParam(value="text",defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
---
+++ src/main/java/com/thiagoribeiro/workshopmongo/resources/resources/util/URL.java
@@ -0,0 +1,15 @@
+package com.thiagoribeiro.workshopmongo.resources.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+	
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
